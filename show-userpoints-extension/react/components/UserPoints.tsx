333730fd522227562e961529aa9e9015786d027a
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-07-19 22:52:44 -0300

    feat: render api response

--- show-userpoints-extension/react/components/UserPoints.tsx
+++ show-userpoints-extension/react/components/UserPoints.tsx
@@ -1,40 +1,47 @@
-import React, { FunctionComponent, ReactElement } from "react";
-// import { ContentWrapper } from 'vtex.my-account-commons'
+import { string } from "prop-types";
+import React, {
+  FunctionComponent,
+  ReactElement,
+  useEffect,
+  useState
+} from "react";
+// import { ContentWrapper } from "vtex.my-account-commons";
 import { Query } from "react-apollo";
 
 import getUserEmail from "../graphql/getUserEmail.gql";
+// import getUserOrders from "../graphql/getUserOrders.gql";
+
+async function fetchUsers() {
+  const usersList = await fetch(
+    "https://bh2wxvl7o0.execute-api.sa-east-1.amazonaws.com/Prod/",
+    {
+      method: "GET"
+    }
+  );
+  const data = await usersList.json();
+
+  return data;
+}
 
 const UserPoints: FunctionComponent<Props> = () => {
+  const [users, setUsers] = useState([{ id: string, name: string }]);
+  useEffect(() => {
+    fetchUsers().then(data => {
+      setUsers(data);
+    });
+  });
   return (
-    <div>
+    <>
       <h1> Meus Pontos </h1>
       <Query query={getUserEmail}>
-        {({ data }: any) => {
-          return (
-            <p>
-              {data && data.profile
-                ? data.profile.email
-                : "não foi possível recuperar seu email"}
-            </p>
-          );
-
-          // return render([
-          //   {
-          //     label: "Loading",
-          //     value: loading.toString()
-          //   },
-          //   {
-          //     label: "Email do Usuário Logado",
-          //     value: data && data.profile ? data.profile.email : "Loading"
-          //   },
-          //   {
-          //     label: "Error",
-          //     value: error
-          //   }
-          // ]);
+        {({ loading }: any) => {
+          if (loading) {
+            return <p> Carregando ...</p>;
+          }
+          return <> {users.map(user => user.id + " ")} </>;
         }}
       </Query>
-    </div>
+    </>
   );
 };
 
